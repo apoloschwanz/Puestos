99cb0da4351daa9c7a57dc8c7c31310daa85e1ec
apoloschwanz <daniel.zabalet@gmail.com>
2016-12-17 20:16:08 -0300

    Arrreglo Combo Carrera

--- clase_carrera.php
+++ clase_carrera.php
@@ -151,6 +151,74 @@
 			$this->mostrar_lista_abm() ;
 		}
 
+	protected function Carga_Sql_Lista()
+	{
+		$this->strsql = ' SELECT ' ;
+		$tf_primero = true ;
+		$tf_filtra_por_codigo = false ;
+		foreach ( $this->lista_campos_lista as $campo )
+		{
+			if ( $tf_primero )
+			{
+				$tf_primero = false;
+				$ts_pk = $campo->nombre() ; // by dz 2016-10-24  $campo['nombre'] ;
+			}
+			else
+				$this->strsql .= ' , ';
+			$this->strsql .= $campo->nombre(); // by dz 2016-10-24  $campo['nombre'] ;
+			//
+			// PK
+			if ( $campo->tipo() == 'pk' )
+			{
+				if ( $campo->busqueda() == true and ! empty( $this->filtro_id) ) 
+					$tf_filtra_por_codigo = true ;
+			}
+		}
+		
+		$this->strsql .= ' FROM '.$this->nombre_fisico_tabla. ' ' ;
+		// by dz 2016-10-24
+		// -->
+		if ( $tf_filtra_por_codigo ) 
+		{
+			$ts_where = ' WHERE ' ;
+			$ts_where .= $ts_pk . ' = '." '".$this->filtro_id."' " ;
+			$this->strsql .= $ts_where ;
+		}
+		//
+		// Filtro por campos de búsqueda
+		if ( ! empty( $this->filtro_gral ) and ! $tf_filtra_por_codigo)
+		{
+			$tn_campos_busqueda = 0 ;
+			$ts_where = ' WHERE ' ;
+			foreach( $this->lista_campos_lista as $campo )
+			{
+				if($campo->busqueda() )
+				{
+					 $tn_campos_busqueda ++ ;
+					 if ( $tn_campos_busqueda > 1 )
+						$ts_where .= ' or ' ;
+					 $ts_where .= $campo->nombre() ;
+					 $ts_where .= ' LIKE ' ;
+					 $ts_where .= " '%" .$this->filtro_gral. "%' " ;
+					 
+				}
+			}
+			if( $tn_campos_busqueda > 0 )
+			{
+				$this->strsql .= $ts_where ;
+			}
+		}
+		// <--
+		// by dz 2016-10-24
+		/*
+		if ( empty( $this->desde ) or $this->desde < 0 )
+			$this->desde = 0 ;
+		if ( $this->cuenta )
+			$this->strsql .= ' LIMIT '. $this->desde . ' , ' . $this->cuenta ;
+		*/
+	}
+
+
 		protected function crear_tabla ()
 		{
 			$this->strsql = "
